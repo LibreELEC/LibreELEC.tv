5f4042ec5771a7cc6ca95522fb77aebb26e36782
Rudi Heitbaum <rudi@heitbaum.com>
2024-09-17 08:13:20 +0000

    pv: update to 1.9.31

--- packages/addons/addon-depends/system-tools-depends/pv/package.mk
+++ packages/addons/addon-depends/system-tools-depends/pv/package.mk
@@ -2,13 +2,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pv"
-PKG_VERSION="1.8.14"
-PKG_SHA256="0cc18811a4809a587d4b11d47691bbc0ad83a5d95d2c2606af74ea7b4a674756"
+PKG_VERSION="1.9.31"
+PKG_SHA256="a35e92ec4ac0e8f380e8e840088167ae01014bfa008a3a9d6506b848079daedf"
 PKG_LICENSE="GNU"
 PKG_SITE="http://www.ivarch.com/programs/pv.shtml"
 PKG_URL="http://www.ivarch.com/programs/sources/pv-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Pipe Viewer can be inserted into any normal pipeline between two processes."
 PKG_BUILD_FLAGS="-sysroot"
-
-PKG_CONFIGURE_OPTS_TARGET="--enable-static-nls"
