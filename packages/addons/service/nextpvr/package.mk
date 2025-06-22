71e57a4486219cd247c9881851a2e7d3431a4ad5
Martin Vallevand <mvallevand@gmail.com>
2025-06-22 09:04:50 -0400

    [le12] Update to 7.0.2
    Update to server https://forums.nextpvr.com/showthread.php?tid=66398

--- packages/addons/service/nextpvr/package.mk
+++ packages/addons/service/nextpvr/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nextpvr"
-PKG_VERSION="7.0.1~Omega"
-PKG_ADDON_VERSION="7.0.1~2"
+PKG_VERSION="7.0.2~Omega"
+PKG_ADDON_VERSION="7.0.2~3"
 PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="NextPVR"
