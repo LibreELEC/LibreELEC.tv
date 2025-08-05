7b1178d4e4ed4712ae22614fa42fd367a4f9720e
Matthias Reichl <hias@horus.com>
2025-08-05 13:30:52 +0200

    libxkbcommon: update to 9d4885d
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/wayland/libxkbcommon/package.mk
+++ packages/wayland/libxkbcommon/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libxkbcommon"
-PKG_VERSION="1.10.0"
-PKG_SHA256="0427585a4d6ca17c9fc1ac4b539bf303348e9080af70c5ea402503bc370a9631"
+PKG_VERSION="9d4885dfb920c79fa157e0c990c63aa6896fc006"
+PKG_SHA256="16d6a02e80193778a2048c7db16d33d3637bc1434e9fba7e35c372ffdd84a373"
 PKG_LICENSE="MIT"
 PKG_SITE="https://xkbcommon.org"
-PKG_URL="https://github.com/xkbcommon/libxkbcommon/archive/refs/tags/xkbcommon-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/xkbcommon/libxkbcommon/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain xkeyboard-config libxml2"
 PKG_LONGDESC="xkbcommon is a library to handle keyboard descriptions."
 
