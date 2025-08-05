a0b47269fd90df2f601ff46e7139cd5836036089
Matthias Reichl <hias@horus.com>
2025-08-05 13:48:48 +0200

    xorgproto: update to ce7786e
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/x11/proto/xorgproto/package.mk
+++ packages/x11/proto/xorgproto/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="xorgproto"
-PKG_VERSION="2024.1"
-PKG_SHA256="372225fd40815b8423547f5d890c5debc72e88b91088fbfb13158c20495ccb59"
+PKG_VERSION="ce7786ebb90f70897f8038d02ae187ab22766ab2"
+PKG_SHA256="a0630df2c8add5773cea7d6f010d17ed0f7a3093782583504fafdfbaa4b98d55"
 PKG_LICENSE="OSS"
 PKG_SITE="https://www.X.org"
-PKG_URL="https://xorg.freedesktop.org/archive/individual/proto/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_URL="https://gitlab.freedesktop.org/xorg/proto/xorgproto/-/archive/master/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain util-macros"
 PKG_LONGDESC="combined X.Org X11 Protocol headers"
 
