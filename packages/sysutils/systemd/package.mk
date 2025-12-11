71c8f0c3bfa8ed56a1b3ea4db18cecaedce82a35
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-11 10:55:21 +0000

    systemd: update to 259-rc3
    Release notes:
    - https://github.com/systemd/systemd/releases

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="systemd"
-PKG_VERSION="13a74f350e0629bedb9e3dbcb53cbcff365af699"
-PKG_SHA256="2507159c0fccfef8c8f9568f3de1679590c020c1c277094be2f85d96acbca220"
+PKG_VERSION="259-rc3"
+PKG_SHA256="89241b11eca4ea5019f75ea5794d356d153c541c166844550d11f140891d6993"
 PKG_LICENSE="LGPL2.1+"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/systemd"
-PKG_URL="https://github.com/systemd/systemd/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/systemd/systemd/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="meson:host ninja:host gcc:host cryptsetup libcap kmod util-linux entropy libidn2 wait-time-sync Jinja2:host"
 PKG_LONGDESC="A system and session manager for Linux, compatible with SysV and LSB init scripts."
 PKG_BUILD_FLAGS="+lto"
