6d692f853b24d008003e7d511a56e08ff2926758
Rudi Heitbaum <rudi@heitbaum.com>
2026-02-24 11:45:16 +0000

    llvm: update to 22.1.0

--- packages/lang/llvm/package.mk
+++ packages/lang/llvm/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="llvm"
-PKG_VERSION="21.1.8"
-PKG_SHA256="4633a23617fa31a3ea51242586ea7fb1da7140e426bd62fc164261fe036aa142"
+PKG_VERSION="22.1.0"
+PKG_SHA256="25d2e2adc4356d758405dd885fcfd6447bce82a90eb78b6b87ce0934bd077173"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="http://llvm.org/"
 PKG_URL="https://github.com/llvm/llvm-project/releases/download/llvmorg-${PKG_VERSION}/llvm-project-${PKG_VERSION/-/}.src.tar.xz"
