e51bb018130cfaa04598aed5421e76954d95f82c
Rudi Heitbaum <rudi@heitbaum.com>
2026-04-28 08:44:19 +0000

    kodi-binary-addons: update to latest versions
    - pvr.teleboy: update 21.0.2-Omega to 21.0.3-Omega
    - pvr.zattoo: update 21.0.6-Omega to 21.0.7-Omega

--- packages/mediacenter/kodi-binary-addons/pvr.teleboy/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.teleboy/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.teleboy"
-PKG_VERSION="21.0.2-Omega"
-PKG_SHA256="d6fa3bfc7b241b092360ddf00f32175e020176c54f3aba9f740066919de01e32"
+PKG_VERSION="21.0.3-Omega"
+PKG_SHA256="d65e3ad48823880ecaa3127046ca31ff8624bc35223d7f2e8c560980071643db"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
