6ee1a47f14e629dc529e023c16a8b00b85c8aab7
Rudi Heitbaum <rudi@heitbaum.com>
2026-05-06 08:55:25 +0000

    pvr.teleboy: update 21.0.4-Omega to 21.0.5-Omega

--- packages/mediacenter/kodi-binary-addons/pvr.teleboy/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.teleboy/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.teleboy"
-PKG_VERSION="21.0.4-Omega"
-PKG_SHA256="579346aec0caf0b5a5609547a6787e70455b8c87ee09fe2d2f5b8d0b2316f9be"
+PKG_VERSION="21.0.5-Omega"
+PKG_SHA256="0fb89305bb01bb9010b1631b6cbffa6afa661c39d97292371807e1ada9cf0948"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
