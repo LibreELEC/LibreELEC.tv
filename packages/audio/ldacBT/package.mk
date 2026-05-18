248ab3597a92bb2e5e58b601f95ac73027f5a35b
EliasTheBigMasterofBullshit <eliascontato@protonmail.com>
2026-05-18 17:56:28 -0300

    packages: audio: ldacBT: Move to EliasTheBigMasterofBullshit's fork

--- packages/audio/ldacBT/package.mk
+++ packages/audio/ldacBT/package.mk
@@ -2,16 +2,16 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="ldacBT"
-PKG_VERSION="2.0.2.3"
+PKG_VERSION="2.0.3.1"
 PKG_SHA256="c02998718f9c4620437d7594b4d121b3ab4c5cfeba8d41fa31dd5c71db09edca"
 PKG_LICENSE="Apache"
-PKG_SITE="https://github.com/EHfive/ldacBT"
-PKG_URL="https://github.com/EHfive/ldacBT/archive/v${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/EliasTheBigMasterofBullshit/ldacBT"
+PKG_URL="https://github.com/EliasTheBigMasterofBullshit/ldacBT/releases/download/workflow_8/ldacBT-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_DEPENDS_UNPACK="libldac"
-PKG_LONGDESC="LDAC Bluetooth encoder library (build tools)"
+PKG_LONGDESC="LDAC Bluetooth encoder/decoder library (build tools)"
 
-PKG_CMAKE_OPTS_TARGET="-DLDAC_SOFT_FLOAT=OFF"
+PKG_CMAKE_OPTS_TARGET="-DLDAC_SOFT_FLOAT=OFF -DAPPLY_PATCHES=FALSE"
 
 post_unpack() {
   rm -rf ${PKG_BUILD}/libldac
