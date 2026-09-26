7111ee2ef44da89f117d5e902c959f3086c4f7f7
Rudi Heitbaum <rudi@heitbaum.com>
2026-09-26 03:26:02 +0000

    scikit-build-core: update to 1.1.0

--- packages/python/devel/scikit-build-core/package.mk
+++ packages/python/devel/scikit-build-core/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="scikit-build-core"
-PKG_VERSION="1.0.3"
-PKG_SHA256="32954222ccaaac8b02a0490eecb9eba38cca2b850f365b576666b2bcae66ce84"
+PKG_VERSION="1.1.0"
+PKG_SHA256="eba9ccd83897d25da54a6c8e0dc8767c4cb0880a88f56bad732ef106d4111a6d"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/scikit-build/scikit-build-core"
 PKG_URL="https://github.com/scikit-build/scikit-build-core/archive/refs/tags/v${PKG_VERSION}.tar.gz"
